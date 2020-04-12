cd937255732697fc0055e3c7a5aed357d0bcef99
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-12 16:20:57 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record){
+  let win = record.find(game => game.result === "W")
+  if(win){
+    return win.year
+  }
+}
